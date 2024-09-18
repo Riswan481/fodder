e96faa8dc04a39102b61ee76c253eaf7b72c0052
[Riswan Tunneling] <vpnscriswan293@gmail.com>
2024-09-18 17:28:02 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -17,9 +17,9 @@ echo -e " "
 read -p " SUBDOMAIN  :  " domen
 echo -e ""
 #domen=lt.`(</dev/urandom tr -dc a-z0-9 | head -c5)`
-DOMAIN=inject.cloud
+DOMAIN=riswan-store.my.id
 sub=${domen}
-dns=${sub}.inject.cloud
+dns=${sub}.riswan-store.my.id
 CF_KEY=e03f30d53ad7ec2ab54327baa5e2da5ab44f0
 CF_ID=mezzqueen293@gmail.com
 set -euo pipefail
